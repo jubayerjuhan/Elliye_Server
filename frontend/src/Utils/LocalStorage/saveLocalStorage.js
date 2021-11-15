c6138d3309b0846ee0822bd56073b7ad63f1d0c5
Jubayer Juhan <33898452+jubayerjuhan@users.noreply.github.com>
2021-11-16 01:31:32 +0600

    Delete frontend directory

--- frontend/src/Utils/LocalStorage/saveLocalStorage.js
+++
@@ -1,8 +0,0 @@
-export function saveLocalStorage(key, value, day) {
-  const now = new Date()
-  const item = {
-    token: value,
-    expiry: now.getTime() + day * 24 * 60 * 60 * 1000,
-  }
-  localStorage.setItem(key, JSON.stringify(item))
-}
