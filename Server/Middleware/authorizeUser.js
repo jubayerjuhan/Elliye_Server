8018aaf04e8baca7e8622a1a953ec84f0f10b504
Jubayer Juhan <davidjuhan23@gmail.com>
2021-12-21 08:44:20 -0500

    authorize user expiry added

--- Server/Middleware/authorizeUser.js
+++ Server/Middleware/authorizeUser.js
@@ -7,9 +7,10 @@ const jwt = require('jsonwebtoken')
 exports.authorizeUser = async (req, res, next) => {
   const authHeader = req.headers.authorization
   const token = authHeader && authHeader.split(' ')[1]
-  const expiry = authHeader && authHeader.split(' ')[2]
-  console.log(authHeader)
+  const expiry = Number(authHeader && authHeader.split(' ')[2])
+
   const expired = Date.now() > expiry;
+  if (expired) return next(new ErrorHandler('JSON Web Token Expired', 401));
   if (!token) return next(new ErrorHandler('Please Login First', 401));
   const decodeData = jwt.verify(token, process.env.JWT_SECRET)
   if (!decodeData) return next(new ErrorHandler('You Messed your jwt token', 404));
